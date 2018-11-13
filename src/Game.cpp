1d5f318c5ea59cc75fe0f18c73e44e69ee9a9bee
the-adam-walker <adamwalker2021@gmail.com>
2018-11-13 14:54:03 -0700

    attempt at main
    nothing special, just some messing around to create a main

---
+++ src/Game.cpp
@@ -0,0 +1,14 @@
+/*
+ * Game.cpp
+ *
+ *  Created on: Nov 13, 2018
+ *      Author: adamw
+ */
+
+#include "Game.h"
+#include "Map.h"
+
+int main() {
+	Map map(10);
+	return 0;
+}
---
+++ src/Game.h
@@ -0,0 +1,14 @@
+/*
+ * Game.h
+ *
+ *  Created on: Nov 13, 2018
+ *      Author: adamw
+ */
+
+#ifndef GAME_H_
+#define GAME_H_
+
+class Game {
+};
+
+#endif /* GAME_H_ */
